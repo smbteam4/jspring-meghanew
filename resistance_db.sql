e63f3d87075a3eb61045681eabab433f9e672bac
Megha <mmegha2234@gmail.com>
2022-04-27 09:30:33 +0530

    source code

---
+++ resistance_db.sql
@@ -0,0 +1,55 @@
+/*
+SQLyog Community Edition- MySQL GUI v8.05 
+MySQL - 5.5.5-10.1.38-MariaDB : Database - resistance
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`resistance` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `resistance`;
+
+/*Table structure for table `resources` */
+
+DROP TABLE IF EXISTS `resources`;
+
+CREATE TABLE `resources` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `resource_name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+/*Data for the table `resources` */
+
+insert  into `resources`(`id`,`resource_name`) values (1,'Water'),(2,'Food'),(3,'Medication'),(4,'Ammunition');
+
+/*Table structure for table `survivor` */
+
+DROP TABLE IF EXISTS `survivor`;
+
+CREATE TABLE `survivor` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  `gender` varchar(20) DEFAULT NULL,
+  `latitude` varchar(255) DEFAULT NULL,
+  `longitude` varchar(255) DEFAULT NULL,
+  `is_infected` tinyint(1) DEFAULT NULL,
+  `resource_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `survivor_resource` (`resource_id`),
+  CONSTRAINT `survivor_resource` FOREIGN KEY (`resource_id`) REFERENCES `resources` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*Data for the table `survivor` */
+
+insert  into `survivor`(`id`,`name`,`age`,`gender`,`latitude`,`longitude`,`is_infected`,`resource_id`) values (1,'SOH',18,'Male','696969S','4543534N',1,1);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
